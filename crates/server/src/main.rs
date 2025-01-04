b71122c76bd853fa0f9a0178f80207cef0f60443
Abby Grobbel <abbygrobbel@gmail.com>
2025-01-04 18:01:42 -0500

    fixes #1

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,10 +1,39 @@
-use miniserve::{Content, Request, Response};
+use miniserve::{http::StatusCode, Content, Request, Response};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Chat {
+    messages: Vec<String>
+}
 
 fn index(_req: Request) -> Response {
     let content = include_str!("../index.html").to_string();
     Ok(Content::Html(content))
 }
 
+fn post_chat(req: Request) -> Response {
+    match req {
+        Request::Post(body) => {
+            let mut chat: Chat = serde_json::from_str(&body)
+                .map_err(|e| StatusCode::BAD_REQUEST)?;
+            
+            chat.messages.push("How does that make you feel?".to_string());
+            
+            let chat_str = serde_json::to_string(&chat)
+                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+            return Ok(Content::Json(chat_str))
+        },
+        _ => {
+            return Err(StatusCode::BAD_REQUEST);
+        }
+    }
+    
+}
+
 fn main() {
-    miniserve::Server::new().route("/", index).run()
+    miniserve::Server::new()
+        .route("/", index)
+        .route("/chat", post_chat)
+        .run()
 }
