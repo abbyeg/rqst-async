649e887e677a0e171e7a884709bd5f22b1c7f9fc
Abby <dev@abbygrobbel.com>
2025-01-13 21:21:11 -0500

    Add support for retrieval-augmented generation (#10)
    * Add support for retrieval-augmented generation
    
    * Add data
    
    * fixes #11
    
    ---------
    
    Co-authored-by: Will Crichton <wcrichto@cs.stanford.edu>
    Co-authored-by: Abby Grobbel <abbygrobbel@gmail.com>

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,9 +1,13 @@
 use miniserve::{http::StatusCode, Content, Request, Response};
 use serde::{Deserialize, Serialize};
-use std::sync::{Arc, LazyLock};
+use std::{
+    path::PathBuf,
+    sync::{Arc, LazyLock},
+};
 use tokio::{
-    join,
+    fs, join,
     sync::{mpsc, oneshot},
+    task::JoinSet,
 };
 
 #[derive(Serialize, Deserialize)]
@@ -11,24 +15,41 @@ struct Chat {
     messages: Vec<String>,
 }
 
+type Payload = (Arc<Vec<String>>, oneshot::Sender<Vec<String>>);
+
 async fn index(_req: Request) -> Response {
     let content = include_str!("../index.html").to_string();
     Ok(Content::Html(content))
 }
 
-async fn query_chat(messages: &Arc<Vec<String>>) -> Vec<String> {
-    type Payload = (Arc<Vec<String>>, oneshot::Sender<Vec<String>>);
-    static SENDER: LazyLock<mpsc::Sender<Payload>> = LazyLock::new(|| {
-        let (tx, mut rx) = mpsc::channel::<Payload>(1024);
-        tokio::spawn(async move {
-            let mut chatbot = chatbot::Chatbot::new(vec![":-)".into(), "^^".into()]);
-            while let Some((messages, responder)) = rx.recv().await {
-                let response = chatbot.query_chat(&messages).await;
-                responder.send(response).unwrap();
-            }
-        });
-        tx
+async fn load_docs(paths: Vec<PathBuf>) -> Vec<String> {
+    let mut doc_futures = paths
+        .into_iter()
+        .map(fs::read_to_string)
+        .collect::<JoinSet<_>>();
+    let mut docs = Vec::new();
+    while let Some(result) = doc_futures.join_next().await {
+        docs.push(result.unwrap().unwrap());
+    }
+    docs
+}
+
+fn chatbot_thread() -> mpsc::Sender<Payload> {
+    let (tx, mut rx) = mpsc::channel::<Payload>(1024);
+    tokio::spawn(async move {
+        let mut chatbot = chatbot::Chatbot::new(vec!["😵‍💫".into(), "🤔".into()]);
+        while let Some((messages, responder)) = rx.recv().await {
+            let paths = chatbot.retrieval_documents(&messages);
+            let docs = load_docs(paths).await;
+            let response = chatbot.query_chat(&messages, &docs).await;
+            responder.send(response).unwrap();
+        }
     });
+    tx
+}
+
+async fn query_chat(messages: &Arc<Vec<String>>) -> Vec<String> {
+    static SENDER: LazyLock<mpsc::Sender<Payload>> = LazyLock::new(chatbot_thread);
 
     let (tx, rx) = oneshot::channel();
     SENDER.send((Arc::clone(messages), tx)).await.unwrap();
